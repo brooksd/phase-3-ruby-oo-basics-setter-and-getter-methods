9622baa714a27541c3a8fde55ad68f22b3efe366
Donald Brooks Arum <brooksd079@gmail.com>
2022-08-17 09:11:44 +0300

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    #Getter Methods
+    def name=(name)
+        @name = name
+    end
 
+    def job=(job)
+        @name=name
+    end
+
+    #Setter Methods
+    def name
+        @name
+    end
+    def job
+        @job
+    end
 end
